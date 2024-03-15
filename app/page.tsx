2686e283b45323cbe29659d1c6a2603ddcc8fbaf
Syphurus <146368398+Syphurus@users.noreply.github.com>
2024-03-15 17:19:09 +0530

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next Js 14 we are comming 1.</h1>
-      <h2 className="h2-bold">Next Js 14 we are comming 2.</h2>
-      <h3 className="h1-bold">Next Js 14 we are comming 3.</h3>
-    </div>
-  );
-}
